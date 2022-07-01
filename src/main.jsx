0fdf73e421bdfc4b26d60446424ac0239077345d
Higashi-Kota <higashi.kota@gri.jp>
2022-07-01 17:59:18 +0900

    add layouting

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,51 @@ import {createRoot} from 'react-dom/client';
 import {css} from '@emotion/css';
 import '@fontsource/kaushan-script';
 import './styles/index.scss';
+import {Button} from '@mui/material';
 
 const App = () => {
-  return null;
+  return (
+    <div
+      className={css`
+        display: grid;
+        place-items: center;
+        min-height: 100vh;
+        width: 100%;
+      `}
+    >
+      <div
+        className={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          gap: 1rem;
+          border: 1px solid;
+        `}
+      >
+        <div
+          className={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 1rem;
+          `}
+        >
+          <Button variant={'outlined'}>Up</Button>
+          <Button variant={'outlined'}>Down</Button>
+        </div>
+        <p
+          className={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          `}
+        >
+          0
+        </p>
+      </div>
+    </div>
+  );
 };
 
 const container = document.getElementById('root');
